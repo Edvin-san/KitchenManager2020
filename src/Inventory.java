7dc069ff706cb5d1729539910e95742a41a010e2
Edvin <edvinlun@kth.se>
2014-03-01 16:08:16 +0100

    safe

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,4 @@
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  * The inventory encapsulated
@@ -11,7 +11,6 @@ public class Inventory {
 	private Connection conn;
 	//TODO
 	//List of prepared statements?
-	//nånting bara
 	
 	
 	public Inventory(Connection inConn) {
@@ -59,7 +58,6 @@ public class Inventory {
 	 */
 	public boolean amountKnown(String prodName) {
 		//TODO
-		//TODO ta bort denna kommentar
 		return false;
 	}
 	
